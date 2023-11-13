846f64bec422f025f0b551a1b3184b4394baeee8
RiverleeO <R.ODriscoll2398@student.leedsbeckett.ac.uk>
2023-11-13 01:47:58 +0000

    Main Command: Clear
    The completed creation of the Clear main command

--- SimpleProgrammingLanguage/SimpleProgrammingLanguage/Commands/Clear.cs
+++ SimpleProgrammingLanguage/SimpleProgrammingLanguage/Commands/Clear.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace SimpleProgrammingLanguage.Commands
 {
+    /// <summary>
+    /// A command that clears the canvas.
+    /// </summary>
     internal class Clear : CommandBase
     {
+        /// <summary>
+        /// Executes the 'clear' command which removes all drawings from the canvas.
+        /// </summary>
+        /// <param name="canvas">The canvas which the rectangle is drawn on.</param>
+        /// <param name="args">No arguments are required for this command.</param>
         public override void ExecuteCommand(Canvas canvas, string[] args)
         {
-
+            canvas.CanvasBox.Invalidate();
         }
     }
 }
