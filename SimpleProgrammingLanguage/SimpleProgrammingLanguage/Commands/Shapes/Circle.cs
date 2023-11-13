dc207a75a43182125d0e72d3172704a0bead6420
RiverleeO <R.ODriscoll2398@student.leedsbeckett.ac.uk>
2023-11-13 01:22:04 +0000

    Circle Shape Command
    The completed creation of the circle shape command

--- SimpleProgrammingLanguage/SimpleProgrammingLanguage/Commands/Shapes/Circle.cs
+++ SimpleProgrammingLanguage/SimpleProgrammingLanguage/Commands/Shapes/Circle.cs
@@ -6,11 +6,55 @@ using System.Threading.Tasks;
 
 namespace SimpleProgrammingLanguage.Commands.Shapes
 {
-    internal class Circle : ShapesBase
+    /// <summary>
+    /// A command to draw a circle on the canvas.
+    /// </summary>
+    public class Circle : ShapesBase
     {
+        /// <summary>
+        /// Executes the circle command, drawing a circle on the canvas with the given radius value.
+        /// </summary>
+        /// <param name="graphics">A graphics object that is used to draw the circle.</param>
+        /// <param name="args">A command argument which gets the radius for the circle.</param>
+        /// <param name="canvas">The canvas which the circle is drawn on.</param>
         public override void ExecuteCommand(Graphics graphics, string[] args, Canvas canvas)
         {
+            Point penPosition = canvas.PenPosition;
+            Pen pen = canvas.DrawPen;
+            TextBox commandBox = canvas.CommandBox;
 
+            if (args.Length >= 1)
+            {
+                if (int.TryParse(args[0], out int circleRadius))
+                {
+                    // Calculates and X and Y position of the pen so that the circle radius is drawn from the center
+                    int x = penPosition.X - circleRadius;
+                    int y = penPosition.Y - circleRadius;
+
+                    // Checks if the filling option has been enabled or disabled (disabled by default)
+                    if (!canvas.Filling)
+                    {
+                        // Draws the circle without any fill
+                        graphics.DrawEllipse(pen, x, y, 2 * circleRadius, 2 * circleRadius);
+                    }
+                    else
+                    {
+                        // Draws the circle with a solid fill
+                        using (SolidBrush brush = new SolidBrush(canvas.FillColour))
+                        {
+                            graphics.FillEllipse(brush, x, y, 2 * circleRadius, 2 * circleRadius);
+                        }
+                    }
+
+                    // Clears the command text box
+                    commandBox.Clear();
+                }
+                else
+                {
+                    // Throws an error message if a valid radius value is not entered
+                    MessageBox.Show("An error occurred when parsing arguments for the 'CIRCLE' command. You must enter a valid radius value.", "Parsing Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
