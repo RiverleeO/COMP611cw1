3fce4c8dd7a35c170293bd03cee8c7ecaa8b381d
RiverleeO <R.ODriscoll2398@student.leedsbeckett.ac.uk>
2024-01-07 23:48:51 +0000

    Reset unit test
    Unit test for reset command to test if:
    *successfully resets pen position to default

---
+++ UnitTests/ResetTest.cs
@@ -0,0 +1,30 @@
+﻿using SimpleProgrammingLanguage;
+using SimpleProgrammingLanguage.Commands;
+using System.Drawing;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// A test class to test the Reset command class.
+    /// </summary>
+    [TestClass]
+    public class ResetTest
+    {
+        /// <summary>
+        /// A test method to verify that the execution of the Reset command successfully resets the pen position to the default value on the canvas.
+        /// </summary>
+        [TestMethod]
+        public void Execute_Reset()
+        {
+            // Arrange
+            var canvas = new Canvas();
+            var resetCmd = new Reset();
+
+            // Act
+            resetCmd.ExecuteCommand(canvas, null);
+
+            // Assert
+            Assert.AreEqual(new Point(50, 50), canvas.PenPosition);
+        }
+    }
+}
