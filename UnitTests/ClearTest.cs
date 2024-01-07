325d3a463ed61579d5a920b472397cdba9024750
RiverleeO <R.ODriscoll2398@student.leedsbeckett.ac.uk>
2024-01-07 23:09:21 +0000

    Clear command UnitTest
    A unit test for the Clear command to verify clearing of the canvas

---
+++ UnitTests/ClearTest.cs
@@ -0,0 +1,35 @@
+﻿using SimpleProgrammingLanguage;
+using SimpleProgrammingLanguage.Commands;
+using System.Drawing;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// A test class to test the Clear command class.
+    /// </summary>
+    [TestClass]
+    public class ClearTest
+    {
+        /// <summary>
+        /// A test method to verify that the execution of the Clear command results in the clearing the canvas.
+        /// </summary>
+        [TestMethod]
+        public void Execute_ClearCanvas()
+        {
+            // Arrange
+            var canvas = new Canvas();
+            var rectangleCmd = new SimpleProgrammingLanguage.Commands.Shapes.Rectangle();
+            var clearCmd = new Clear();
+            string[] cmdArgs = { "45", "75" };
+            var graphics = Graphics.FromImage(new Bitmap(150, 150));
+
+            // Act
+            rectangleCmd.ExecuteCommand(graphics, cmdArgs, canvas);
+            clearCmd.ExecuteCommand(canvas, null);
+
+            // Assert
+            Assert.IsTrue(string.IsNullOrEmpty(canvas.CommandBox.Text));
+            Assert.IsFalse(rectangleCmd.error);
+        }
+    }
+}
