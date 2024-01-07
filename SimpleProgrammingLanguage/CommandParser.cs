294cde11b910203340ff71a2f7ce069799308cce
RiverleeO <R.ODriscoll2398@student.leedsbeckett.ac.uk>
2024-01-07 22:54:46 +0000

    New CommandParser
    Creation of new CommandParser

--- SimpleProgrammingLanguage/CommandParser.cs
+++ SimpleProgrammingLanguage/CommandParser.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SimpleProgrammingLanguage.Commands
+namespace SimpleProgrammingLanguage
 {
     /// <summary>
     /// 
---
+++ SimpleProgrammingLanguage/Commands/BaseCommandParser.cs
@@ -0,0 +1,23 @@
+﻿using SimpleProgrammingLanguage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace SimpleProgrammingLanguage.Commands
+{
+    /// <summary>
+    /// A class that represents the command parser which is responsible for executing commands and arguments.
+    /// </summary>
+    abstract public class BaseCommandParser
+    {
+        /// <summary>
+        /// A method that executes given commands and arguments.
+        /// </summary>
+        /// <param name="canvas">The canvas on which the command is executed against.</param>
+        /// <param name="args">The arguments that accompany the command (if any).</param>
+        abstract public void ExecuteCommand(Canvas canvas, string[] args);
+    }
+}
