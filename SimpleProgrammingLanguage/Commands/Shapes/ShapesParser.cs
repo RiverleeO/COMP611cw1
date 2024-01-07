68b4785c68acd903be50f7b460495c5efee902cc
RiverleeO <R.ODriscoll2398@student.leedsbeckett.ac.uk>
2024-01-07 23:01:42 +0000

    Shapes Parser changes
    Removed old Shapes Parser and added new one

---
+++ SimpleProgrammingLanguage/Commands/Shapes/ShapesParser.cs
@@ -0,0 +1,23 @@
+﻿using SimpleProgrammingLanguage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProgrammingLanguage.Commands.Shapes
+{
+    /// <summary>
+    /// A class that represents the shapes command parser which is responsible for executing commands and arguments related to shapes.
+    /// </summary>
+    abstract public class ShapesParser
+    {
+        /// <summary>
+        ///  A method that executes given commands and arguments specific to graphics.
+        /// </summary>
+        /// <param name="graphics">The graphics object which is used for drawing.</param>
+        /// <param name="args">The arguments that accompany the command (if any).</param>
+        /// <param name="canvas">The canvas on which the command is executed against.</param>
+        abstract public void ExecuteCommand(Graphics graphics, string[] args, Canvas canvas);
+    }
+}
