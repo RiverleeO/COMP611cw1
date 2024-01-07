f7673732621d36b32bddae6ccb43d59498dc379d
RiverleeO <R.ODriscoll2398@student.leedsbeckett.ac.uk>
2024-01-07 23:13:00 +0000

    CommandParser unit test
    A unit test for the CommandParser class checking if:
    *commands with no arguments parse successfully
    *parser correctly throws warning when a command/argument is not parsed
    *correctly parses with single argument
    *correctly parses with multiple argument

---
+++ UnitTests/CommandParserTest.cs
@@ -0,0 +1,78 @@
+﻿using SimpleProgrammingLanguage;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// A test class to test the CommandParser class.
+    /// </summary>
+    [TestClass]
+    public class CommandParserTest
+    {
+        /// <summary>
+        /// A test method to verify that commands with no arguments parse successfully.
+        /// </summary>
+        [TestMethod]
+        public void ParseCommand_CommandWithEmptyArgument()
+        {
+            // Arrange
+            string cmd = "CLEAR";
+
+            // Act
+            var cmdParser = new CommandParser(cmd);
+
+            // Assert
+            Assert.AreEqual("CLEAR", cmdParser.Cmd); // Expects the 'CLEAR' command
+            CollectionAssert.AreEqual(new string[0], cmdParser.Args); // Expects empty args
+        }
+
+        /// <summary>
+        /// A test method to verify that the parser correctly throws a warning when no command or arguments are parsed.
+        /// </summary>
+        [TestMethod]
+        public void ParseCommand_WithEmptyCommandAndArgument()
+        {
+            // Arrange
+            string cmd = "";
+
+            // Act
+            var cmdParser = new CommandParser(cmd);
+
+            // Assert
+            Assert.AreEqual("", cmdParser.Cmd); // Expects empty command
+            CollectionAssert.AreEqual(new string[0], cmdParser.Args); // Expects empty args
+        }
+
+        /// <summary>
+        /// A test method to verify that the parser correctly parses commands with a single argument.
+        /// </summary>
+        [TestMethod]
+        public void ParseCommand_CommandWithSingleArgument()
+        {
+            // Arrange
+            string cmd = "PEN BLUE";
+
+            // Act
+            var cmdParser = new CommandParser(cmd);
+
+            // Assert
+            Assert.AreEqual("PEN", cmdParser.Cmd);
+        }
+
+        /// <summary>
+        /// A test method to verify that the parser correctly parses commands with multiple arguments.
+        /// </summary>
+        [TestMethod]
+        public void ParseCommandAndArgs()
+        {
+            // Arrange
+            string cmd = "DRAW 45 75";
+
+            // Act
+            var cmdParser = new CommandParser(cmd);
+
+            // Assert
+            Assert.AreEqual("DRAW", cmdParser.Cmd); // Expects the 'DRAW' command
+            CollectionAssert.AreEqual(new string[] { "45", "75" }, cmdParser.Args); // Expects arguments with XY coords of 45 and 75
+        }
+    }
+}
