17741fe7b95f5165d2042440ba35786831770997
RiverleeO <R.ODriscoll2398@student.leedsbeckett.ac.uk>
2024-01-07 23:16:00 +0000

    DrawTo Unit test
    A unit test of the DrawTo command to check if:
    *throws error message when invalid coordinates are provided
    *executes successfully with correct coordinates

---
+++ UnitTests/DrawToTest.cs
@@ -0,0 +1,50 @@
+﻿using SimpleProgrammingLanguage;
+using SimpleProgrammingLanguage.Commands;
+using System.Drawing;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// A test class to test the DrawTo command class.
+    /// </summary>
+    [TestClass]
+    public class DrawToTest
+    {
+        /// <summary>
+        /// A test method to verify that the DrawTo command throws an error message when invalid coordinates are provided.
+        /// </summary>
+        [TestMethod]
+        public void Execute_DrawTo_WithInvalidCoords()
+        {
+            // Arrange
+            var canvas = new Canvas();
+            var drawToCmd = new DrawTo();
+            string[] args = { "45", "invalidYCoord" };
+
+            // Act
+            drawToCmd.ExecuteCommand(canvas, args);
+
+            // Assert
+            Assert.IsTrue(drawToCmd.error);
+        }
+
+        /// <summary>
+        /// A test method to verify that the DrawTo command executes successfully when valid coordinates are provided.
+        /// </summary>
+        [TestMethod]
+        public void Execute_DrawTo_WithValidCoords()
+        {
+            // Arrange
+            var canvas = new Canvas();
+            var drawToCmd = new DrawTo();
+            string[] args = { "45", "75" };
+
+            // Act
+            drawToCmd.ExecuteCommand(canvas, args);
+
+            //Assert
+            Assert.AreEqual(new Point(45, 75), canvas.PenPosition);
+            Assert.IsFalse(drawToCmd.error);
+        }
+    }
+}
