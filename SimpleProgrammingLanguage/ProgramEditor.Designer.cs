f1da45d77e3d991e6d3ab1de0cb8b1ae34105c78
RiverleeO <R.ODriscoll2398@student.leedsbeckett.ac.uk>
2024-04-05 00:44:14 +0100

    Add the ProgramEditor form class which allows opening, saving, editing and running custom program code

---
+++ SimpleProgrammingLanguage/ProgramEditor.Designer.cs
@@ -0,0 +1,253 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SimpleProgrammingLanguage
+{
+    partial class ProgramEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            editorMenuStrip = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            newToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            saveAsToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            undoToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator2 = new ToolStripSeparator();
+            cutToolStripMenuItem = new ToolStripMenuItem();
+            copyToolStripMenuItem = new ToolStripMenuItem();
+            pasteToolStripMenuItem = new ToolStripMenuItem();
+            deleteToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator3 = new ToolStripSeparator();
+            findToolStripMenuItem = new ToolStripMenuItem();
+            programToolStripMenuItem = new ToolStripMenuItem();
+            runToolStripMenuItem = new ToolStripMenuItem();
+            checkSyntaxToolStripMenuItem = new ToolStripMenuItem();
+            rtbProgText = new RichTextBox();
+            editorMenuStrip.SuspendLayout();
+            SuspendLayout();
+            // 
+            // editorMenuStrip
+            // 
+            editorMenuStrip.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, programToolStripMenuItem });
+            editorMenuStrip.Location = new Point(0, 0);
+            editorMenuStrip.Name = "editorMenuStrip";
+            editorMenuStrip.Size = new Size(1045, 24);
+            editorMenuStrip.TabIndex = 0;
+            editorMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { newToolStripMenuItem, openToolStripMenuItem, saveToolStripMenuItem, saveAsToolStripMenuItem, toolStripSeparator1, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            newToolStripMenuItem.Name = "newToolStripMenuItem";
+            newToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+N";
+            newToolStripMenuItem.Size = new Size(195, 22);
+            newToolStripMenuItem.Text = "New";
+            newToolStripMenuItem.Click += newToolStripMenuItem_Click;
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+O";
+            openToolStripMenuItem.Size = new Size(195, 22);
+            openToolStripMenuItem.Text = "Open...";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Enabled = false;
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+S";
+            saveToolStripMenuItem.Size = new Size(195, 22);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+Shift+S";
+            saveAsToolStripMenuItem.Size = new Size(195, 22);
+            saveAsToolStripMenuItem.Text = "Save As...";
+            saveAsToolStripMenuItem.Click += saveAsToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(192, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(195, 22);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { undoToolStripMenuItem, toolStripSeparator2, cutToolStripMenuItem, copyToolStripMenuItem, pasteToolStripMenuItem, deleteToolStripMenuItem, toolStripSeparator3, findToolStripMenuItem });
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(39, 20);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // undoToolStripMenuItem
+            // 
+            undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            undoToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+Z";
+            undoToolStripMenuItem.Size = new Size(144, 22);
+            undoToolStripMenuItem.Text = "Undo";
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(141, 6);
+            // 
+            // cutToolStripMenuItem
+            // 
+            cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            cutToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+X";
+            cutToolStripMenuItem.Size = new Size(144, 22);
+            cutToolStripMenuItem.Text = "Cut";
+            // 
+            // copyToolStripMenuItem
+            // 
+            copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            copyToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+C";
+            copyToolStripMenuItem.Size = new Size(144, 22);
+            copyToolStripMenuItem.Text = "Copy";
+            // 
+            // pasteToolStripMenuItem
+            // 
+            pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            pasteToolStripMenuItem.ShortcutKeyDisplayString = "Ctrl+V";
+            pasteToolStripMenuItem.Size = new Size(144, 22);
+            pasteToolStripMenuItem.Text = "Paste";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            deleteToolStripMenuItem.ShortcutKeyDisplayString = "Del";
+            deleteToolStripMenuItem.Size = new Size(144, 22);
+            deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // toolStripSeparator3
+            // 
+            toolStripSeparator3.Name = "toolStripSeparator3";
+            toolStripSeparator3.Size = new Size(141, 6);
+            // 
+            // findToolStripMenuItem
+            // 
+            findToolStripMenuItem.Name = "findToolStripMenuItem";
+            findToolStripMenuItem.Size = new Size(144, 22);
+            findToolStripMenuItem.Text = "Find...";
+            // 
+            // programToolStripMenuItem
+            // 
+            programToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { runToolStripMenuItem, checkSyntaxToolStripMenuItem });
+            programToolStripMenuItem.Name = "programToolStripMenuItem";
+            programToolStripMenuItem.Size = new Size(65, 20);
+            programToolStripMenuItem.Text = "Program";
+            // 
+            // runToolStripMenuItem
+            // 
+            runToolStripMenuItem.Name = "runToolStripMenuItem";
+            runToolStripMenuItem.Size = new Size(145, 22);
+            runToolStripMenuItem.Text = "Run";
+            runToolStripMenuItem.Click += runToolStripMenuItem_Click;
+            // 
+            // checkSyntaxToolStripMenuItem
+            // 
+            checkSyntaxToolStripMenuItem.Name = "checkSyntaxToolStripMenuItem";
+            checkSyntaxToolStripMenuItem.Size = new Size(145, 22);
+            checkSyntaxToolStripMenuItem.Text = "Check Syntax";
+            // 
+            // rtbProgText
+            // 
+            rtbProgText.AcceptsTab = true;
+            rtbProgText.BorderStyle = BorderStyle.None;
+            rtbProgText.Font = new Font("Consolas", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            rtbProgText.HideSelection = false;
+            rtbProgText.Location = new Point(46, 27);
+            rtbProgText.Name = "rtbProgText";
+            rtbProgText.ShowSelectionMargin = true;
+            rtbProgText.Size = new Size(999, 621);
+            rtbProgText.TabIndex = 1;
+            rtbProgText.Text = "";
+            // 
+            // ProgramEditor
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1045, 647);
+            Controls.Add(rtbProgText);
+            Controls.Add(editorMenuStrip);
+            KeyPreview = true;
+            MainMenuStrip = editorMenuStrip;
+            Name = "ProgramEditor";
+            Text = "Program Editor";
+            Load += ProgramEditor_Load;
+            editorMenuStrip.ResumeLayout(false);
+            editorMenuStrip.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip editorMenuStrip;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem newToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem undoToolStripMenuItem;
+        private ToolStripMenuItem cutToolStripMenuItem;
+        private ToolStripMenuItem copyToolStripMenuItem;
+        private ToolStripMenuItem pasteToolStripMenuItem;
+        private ToolStripMenuItem deleteToolStripMenuItem;
+        private ToolStripMenuItem programToolStripMenuItem;
+        private ToolStripMenuItem runToolStripMenuItem;
+        private ToolStripMenuItem checkSyntaxToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripSeparator toolStripSeparator3;
+        private ToolStripMenuItem findToolStripMenuItem;
+        private RichTextBox rtbProgText;
+    }
+}
---
+++ SimpleProgrammingLanguage/ProgramEditor.cs
@@ -0,0 +1,179 @@
+﻿using SimpleProgrammingLanguage;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleProgrammingLanguage
+{
+    /// <summary>
+    /// A class that represents the Program Editor form, which is used to open, save, edit and run program code.
+    /// </summary>
+    public partial class ProgramEditor : Form
+    {
+        private Canvas canvas; // the canvas object
+
+        private bool progSaveState; // defines the save state of the program code file
+        private string progName; // defines the name of the saved program code file
+
+        /// <summary>
+        /// Initialises a new instance of the ProgramEditor class and references the canvas object.
+        /// </summary>
+        /// <param name="canvas">The canvas object that is drawn on.</param>
+        public ProgramEditor(Canvas canvas)
+        {
+            InitializeComponent();
+            this.canvas = canvas;
+        }
+
+        /// <summary>
+        /// The event handler that listens for the load event of the ProgramEditor form.
+        /// </summary>
+        private void ProgramEditor_Load(object sender, EventArgs e)
+        {
+            progSaveState = false; // sets the save state of the program code file to false
+            progName = ""; // sets the name of the program code file to a blank string
+        }
+
+        /// <summary>
+        /// The event handler that listens for a click event of the 'New' button within the 'File' tool strip menu.
+        /// </summary>
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (progSaveState == false) // if the save state is false, the user is warned that recent changes haven't been saved and whether they wish to continue
+            {
+                DialogResult dialogResult = MessageBox.Show("You have not saved recent changes. Are you sure you want to close the current program?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (dialogResult == DialogResult.Yes) // if yes, the editor is cleared
+                {
+                    saveToolStripMenuItem.Enabled = false;
+                    rtbProgText.Clear();
+                }
+            }
+            else // if save state is true, then the editor is cleared
+            {
+                saveToolStripMenuItem.Enabled = false;
+                rtbProgText.Clear();
+            }
+        }
+
+        /// <summary>
+        /// The event handler that listens for a click event of the 'Open' button within the 'File' tool strip menu.
+        /// </summary>
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openDialog = new OpenFileDialog())
+            {
+                openDialog.Filter = "Pen Program|*.prog";
+                openDialog.Title = "Open Program File";
+
+                if (openDialog.ShowDialog() == DialogResult.OK)
+                {
+                    progName = openDialog.FileName;
+
+                    try
+                    {
+                        string progCont = File.ReadAllText(progName);
+                        rtbProgText.Text = progCont;
+                        saveToolStripMenuItem.Enabled = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"An error occured while attempting to open the program file. /nException: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The event haandler that listens for a click event of the 'Save' button within the 'File' tool strip menu.
+        /// </summary>
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.WriteAllText(progName, rtbProgText.Text);
+                progSaveState = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occured while saving the file. \nException: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        /// <summary>
+        /// The event handler that listens for a click event of the 'Save As' button within the 'File' tool strip menu.
+        /// </summary>
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveAsDialog = new SaveFileDialog())
+            {
+                saveAsDialog.Filter = "Pen Program|*.prog";
+                saveAsDialog.Title = "Save Script File";
+
+                if (saveAsDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string fileName = saveAsDialog.FileName;
+                    try
+                    {
+                        File.WriteAllText(fileName, rtbProgText.Text);
+                        MessageBox.Show("Program saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        progSaveState = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"An error occured while saving the file. \nException: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The event handler that listens for a click event of the 'Exit' button within the 'File' tool strip menu.
+        /// </summary>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (progSaveState == false)
+            {
+                DialogResult dialogResult = MessageBox.Show("You have not saved recent changes. Are you sure you want to close the editor?", "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    saveToolStripMenuItem.Enabled = false;
+                    this.Close();
+                }
+            }
+            else
+            {
+                saveToolStripMenuItem.Enabled = false;
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// The event handler that listens for a click event of the 'Run' button within the 'Program' tool strip menu
+        /// </summary>
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string progCont = rtbProgText.Text;
+
+            ProgramHandler programHandler = new ProgramHandler(canvas);
+            programHandler.ExecuteProgram(progCont);
+        }
+
+        /// <summary>
+        /// The event handler that listens for a KeyDown event within the RichTextBox editor.
+        /// </summary>
+        private void rtbProgText_KeyDown(object sender, KeyEventArgs e)
+        {
+            progSaveState = false;
+            Console.WriteLine(e.KeyData);
+        }
+    }
+}
